48eb3224107d159818e9ca3f869c6b4a02f7efc2
LJason <ljason@ljason.cn>
2023-06-09 02:24:53 +0800

    通过 adb 自动填写
    Signed-off-by: LJason <ljason@ljason.cn>

--- README.md
+++ README.md
@@ -6,7 +6,37 @@
 ![GitHub forks](https://img.shields.io/github/forks/LJason77/Sudoku?style=social)
 ![GitHub Repo stars](https://img.shields.io/github/stars/LJason77/Sudoku?style=social)
 
-这是一个模板。
+## 依赖
+
+```shell
+# 依赖
+sudo apt install -y libleptonica-dev libtesseract-dev tesseract-ocr bc
+```
+
+需要安装 adb 并且配置好环境变量。
+
+## 训练模型
+
+```shell
+git clone --depth=1 https://github.com/tesseract-ocr/tesstrain.git ; cd tesstrain
+pip3 install -r requirements.txt
+make tesseract-langdata
+mkdir data/num-ground-truth/
+# 将 1-9 的图片（每个数字一张图片） 放到 data/num-ground-truth/ 中
+for i in {1..9}; do echo $i > data/num-ground-truth/$i.gt.txt; done
+mkdir data/tessdata
+wget -c 'https://raw.githubusercontent.com/tesseract-ocr/tessdata_best/main/eng.traineddata' -O data/tessdata/eng.traineddata
+git clone --depth=1 https://github.com/tesseract-ocr/langdata_lstm.git data/langdata
+make training MODEL_NAME=num START_MODEL=eng TESSDATA=data/tessdata
+sudo mv data/num.traineddata /usr/share/tesseract-ocr/4.00/tessdata/
+```
+
+## 运行
+
+```shell
+# 先截图再运行
+adb exec-out screencap -p > screenshot.png ; cargo run --release
+```
 
 ## 许可
 
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
 #![deny(clippy::pedantic)]
 
-// const SUDOKU: [[u8; 9]; 9] = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     // -------------------------
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     // -------------------------
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-// ];
-const SUDOKU: [[u8; 9]; 9] = [
-    [9, 2, 0, 0, 1, 0, 3, 0, 0],
-    [8, 5, 0, 0, 9, 0, 0, 2, 0],
-    [0, 0, 3, 0, 0, 0, 0, 0, 0],
-    // -------------------------
-    [0, 0, 0, 0, 0, 2, 0, 0, 0],
-    [3, 0, 0, 0, 0, 1, 6, 0, 0],
-    [1, 9, 7, 0, 0, 0, 2, 5, 0],
-    // -------------------------
-    [0, 0, 0, 5, 0, 9, 0, 6, 2],
-    [0, 8, 5, 0, 2, 0, 4, 0, 0],
-    [0, 0, 9, 7, 4, 0, 0, 3, 0],
+use std::{process::Command, thread::sleep, time::Duration};
+
+use image::imageops;
+
+static mut SUDOKU: [[u8; 9]; 9] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 // 水平方向
 static mut HORIZONTAL: [u16; 9] = [0; 9];
@@ -35,42 +24,78 @@ static mut SQUARE: [u16; 9] = [0; 9];
 // 是否结束
 static mut FINISHED: bool = false;
 
+// 计算次数
+static mut COUNT: u32 = 0;
+
+#[allow(clippy::cast_possible_truncation)]
 fn main() {
-    // 获得空格的位置
+    // 暂停
+    touch(1000, 365);
+    // 打开图片
+    let mut image = image::open("screenshot.png").unwrap().into_rgb8();
+    // 裁剪图片
+    let width = 1044;
+    let height = 1044;
+    let mut buffer = imageops::crop(&mut image, 20, 443, width, height).to_image();
+
     let mut spaces: Vec<(usize, usize)> = Vec::new();
     for i in 0..9 {
         for j in 0..9 {
-            let n = SUDOKU[i][j];
-            if n == 0 {
-                spaces.push((i, j));
-            } else {
+            let x = j * 116;
+            let y = i * 116;
+            let square = imageops::crop(&mut buffer, x as u32, y as u32, 110, 110).to_image();
+            let result = tesseract::ocr_from_frame(&square, 110, 110, 3, 110 * 3, "num");
+            if let Ok(n) = result.unwrap().trim().parse::<u8>() {
                 unsafe {
+                    SUDOKU[i][j] = n;
                     HORIZONTAL[i] += 1 << n;
                     VERTICAL[j] += 1 << n;
                     SQUARE[(i / 3) * 3 + j / 3] += 1 << n;
                 }
+            } else {
+                spaces.push((i, j));
             }
         }
     }
 
     // 开始递归
-    let mut sudoku = SUDOKU;
     unsafe {
+        let mut sudoku = SUDOKU;
         fill(0, &mut sudoku, &spaces);
-    }
+        // 打印结果
+        print_result(&sudoku);
 
-    // 打印结果
-    print_result(&sudoku);
+        // 打印次数
+        println!("{COUNT}");
+
+        // 恢复游戏
+        touch(540, 1700);
+        sleep(Duration::from_millis(500));
+        // 填入答案
+        for (i, j) in spaces {
+            // 空格坐标
+            let x = 15 + 56 + j * 116;
+            let y = 442 + 56 + i * 116;
+            touch(x, y);
+            // 填入数字
+            let n = sudoku[i][j] as usize;
+            let x: usize = 15 + 56 + (n - 1) * 116;
+            let y = 2000;
+            touch(x, y);
+        }
+    }
 }
 
 // 打印结果
 fn print_result(sudoku: &[[u8; 9]; 9]) {
+    sleep(Duration::from_millis(10));
+    Command::new("clear").status().expect("failed to execute process");
     for (i, row) in sudoku.iter().enumerate() {
         for (j, col) in row.iter().enumerate() {
             if j % 3 == 0 {
                 print!("|");
             }
-            print!("{} ", col);
+            print!("{col} ");
         }
         if i % 3 == 2 {
             print!("\n---------------------");
@@ -100,10 +125,24 @@ unsafe fn fill(pos: usize, sudoku: &mut [[u8; 9]; 9], spaces: &Vec<(usize, usize
         HORIZONTAL[i] += 1 << n;
         VERTICAL[j] += 1 << n;
         SQUARE[square] += 1 << n;
+        COUNT += 1;
         fill(pos + 1, sudoku, spaces);
         HORIZONTAL[i] -= 1 << n;
         VERTICAL[j] -= 1 << n;
         SQUARE[square] -= 1 << n;
         n += 1;
     }
+}
+
+// 模拟 Android 点击
+fn touch(x: usize, y: usize) {
+    Command::new("adb")
+        .arg("shell")
+        .arg("input")
+        .arg("tap")
+        .arg(format!("{x}"))
+        .arg(format!("{y}"))
+        .output()
+        .expect("failed to execute process");
+    sleep(Duration::from_millis(15));
 }
